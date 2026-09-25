3717b15802ed63db06fb57e1ce3b54286afd8962
HariniPrabhaBV <hariniprabha755@gmail.com>
2026-09-25 22:27:07 +0530

    Implement plusOne method to increment an array
    Input: digits = [1,2,3]
    Output: [1,2,4]

---
+++ Plus One.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] plusOne(int[] nums) {
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<9){
+                nums[i]++;
+                return nums;
+            }
+            nums[i]=0;
+        }
+        int []arr=new int[nums.length+1];
+        arr[0]=1;
+        return arr;
+    }
+}
